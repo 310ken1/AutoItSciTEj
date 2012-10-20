1f1641215e643506268d70a43e29df9beb44bcda
Kenichi Sato <310dev@gmail.com>
2012-10-20 17:56:05 +0900

    新規作成.
    メニュー と ヘルプ の日本語化を行う.
    SciTE4AutoIt3 を参考に, 設定ファイル および Tidy.exe（ソースコード整形ツール）を組み込んでいる.
    また, ヘルプについては, AutoIt v3 ドキュメント 日本語訳プロジェクト の 日本語ヘルプ を組み込んでいる.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-AutoItSciTEj
+﻿AutoItSciTEj
 ============
 
-AutoIt の日本語開発環境
+◆概要
+-----
+AutoItSciTEjは、[SciTE](http://www.scintilla.org/SciTE.html) をベースにした、 [AuotIt](http://www.autoitscript.com/site/autoit/) の日本語開発環境です。
+
+◆ライセンス
+----------
+ライセンスは、[パブリックドメイン](http://ja.wikipedia.org/wiki/%E3%83%91%E3%83%96%E3%83%AA%E3%83%83%E3%82%AF%E3%83%89%E3%83%A1%E3%82%A4%E3%83%B3)です。  
+ただし、本プロダクトに含まれる次のソフトウェアおよびファイルについては、各ライセンスに従います。
+
+- [SciTE](http://www.scintilla.org/SciTE.html) [(License)](http://www.scintilla.org/License.txt)
+- [SciTE4AutoIt3](http://www.autoitscript.com/site/autoit-script-editor/) [(License)](http://www.autoitscript.com/autoit3/docs/license.htm)
+- [Lua](http://www.lua.org/) [(License)](http://www.lua.org/license.html)
+- [hhh.exe](http://www.vector.co.jp/soft/winnt/writing/se454186.html)
+- [AutoIt3j.chm](http://blog.livedoor.jp/blackcode/archives/1224220.html) / [UDFs3j.chm](http://blog.livedoor.jp/blackcode/archives/1371819.html) (License:chmファイル参照)
---
+++ lua/SciTEStartup.lua
@@ -0,0 +1,32 @@
+--------------------------------------------------------------------------------
+-- SciTE startup script.
+--------------------------------------------------------------------------------
+
+-- A table listing all loaded files.
+LoadLuaFileList = { }
+
+--------------------------------------------------------------------------------
+-- LoadLuaFile(file, directory)
+--
+-- Helper function for easily loading Lua files.
+--
+-- Parameters:
+--	file - The name of a Lua file to load.
+--	directory - If specified, file is looked for in that directory.  By default,
+-- 		this directory is $(SciTEDefaultHome)\Lua.
+--------------------------------------------------------------------------------
+function LoadLuaFile(file, directory)
+	if directory == nil then
+		directory = props["SciteDefaultHome"] .. "\\Lua\\"
+	end
+	table.insert(LoadLuaFileList, directory .. file)
+	dofile(directory .. file)
+end	-- LoadLuaFile()
+
+-- Load all the Lua files.
+LoadLuaFile("Class.lua")	-- Always load first.
+LoadLuaFile("Common.lua")	-- Always load second.
+LoadLuaFile("AutoItPixmap.lua")
+
+-- Start up the events (Calls OnStartup()).
+EventClass:BeginEvents()
